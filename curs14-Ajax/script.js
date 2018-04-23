57765ff19c8ede869e2ff19d99ba6954edcb16b8
anca.balc <anca.balc@scoalainformala.ro>
2018-04-23 05:19:21 -0400

    Ajax

---
+++ curs14-Ajax/script.js
@@ -0,0 +1,19 @@
+
+$.ajax('https://jsonplaceholder.typicode.com/posts');
+
+$.ajax('https://jsonplaceholder.typicode.com/posts', {
+  success: function(response) {
+    console.log('Am primit lista de posts');
+    console.log('Am primit lista de posts', response);
+  }
+});
+
+$.ajax('https://jsonplaceholder.typicode.com/posts/3');
+$.ajax('https://jsonplaceholder.typicode.com/posts/3', {
+ method: 'GET'
+});
+
+
+
+
+
